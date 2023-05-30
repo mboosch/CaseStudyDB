7f49f4d31cdf74c6e7b8e061a819ccc9f4979e20
matthias <matthias_boosch@yahoo.de>
2023-05-30 12:45:04 +0200

    init: Big Bang

---
+++ src/main/java/de/casestudy/casestudydb/CaseStudyDbApplication.java
@@ -0,0 +1,13 @@
+package de.casestudy.casestudydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudyDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/casestudy/casestudydb/CaseStudyDbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.casestudy.casestudydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
